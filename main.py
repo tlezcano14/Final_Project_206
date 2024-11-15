36355ca5a53cc4104b780af08bb69a6278a51d53
Gabriel Oliveira <djalmaoliveira@MacBook-Air-321.local>
2024-11-15 18:55:26 -0500

    intro

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Gabriel Oliveira, Thomas Lezcano
 # Final Project 
+
+import requests
+import json
+import lyricsgenius
+token = "-SPqPS1Wq_0zYs_L-31AVu0N_7Bx2-UcEmFQZAYs0CQ5zEeQKq083QV-VK0zLNHt"
+genius = lyricsgenius.Genius(token)
+
+song = genius.search_song("Sicko Mode", "Travis Scott")
+
+# Print the song's lyrics
+if song:
+    print(song.lyrics)
+else:
+    print("Song not found.")
